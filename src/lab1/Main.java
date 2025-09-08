d33d5d46885d66fbb089951572bada4609d68133
Victor Polowy <victor.polowy@mail.utoronto.ca>
2025-09-08 16:12:52 -0400

    added Main

---
+++ src/lab1/Main.java
@@ -0,0 +1,11 @@
+package lab1;
+
+public class Main{
+    public static void main(String[] args) {
+        FizzBuzz fb = new FizzBuzz();
+
+        FizzBuzz.doFizzBuzz(5);
+
+        FizzBuzz.main(args);
+    }
+}
